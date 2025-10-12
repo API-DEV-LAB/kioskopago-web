fa64880ba2e13084c930d3f4d7129969636b47ce
efrencodes <efrencodes@gmail.com>
2025-10-11 21:23:31 -0700

    feat: Config axios

---
+++ lib/axios.ts
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+export const API = axios.create({
+	baseURL: process.env.NEXT_PUBLIC_API_URL,
+	timeout: 10000,
+	headers: {
+		'Content-Type': 'application/json',
+		Accept: 'application/json',
+		'App-Version': 'v1.0.0',
+	},
+})
