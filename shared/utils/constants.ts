ba508f5cf637a29f3b4025fc708e459933da4a26
Efren Martinez <efrencodes@gmail.com>
2025-10-23 16:36:58 +0000

    feat: Add MENU_LOGGED_IN constant

--- shared/utils/constants.ts
+++ shared/utils/constants.ts
@@ -15,3 +15,8 @@ export const PHONE_MAX = 10
 export const PHONE_PLACEHOLDER = '(442) 724 210'
 export const CODE_VERIFICATION_MAX = 6
 export const FIELD_REQUIRED = 'Campo requerido'
+export const MENU_LOGGED_IN = [
+	{ name: ROUTES_APP.HOME.name, href: ROUTES_APP.HOME.path },
+	{ name: ROUTES_APP.HISTORIAL.name, href: ROUTES_APP.HISTORIAL.path },
+	{ name: ROUTES_APP.PROFILE.name, href: ROUTES_APP.PROFILE.path },
+]
