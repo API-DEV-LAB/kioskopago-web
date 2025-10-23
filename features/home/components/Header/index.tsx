b1176394886f1c14ffcf7cd919569534acf446f3
Efren Martinez <efrencodes@gmail.com>
2025-10-23 16:40:15 +0000

    feat: Add responsive to header

--- features/home/components/Header/index.tsx
+++ features/home/components/Header/index.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
-import HeaderMenu from '@/features/home/components/HeaderMenu'
+import DesktopNavigation from '@/features/home/components/DesktopNavigation'
+import MobileNavigation from '@/features/home/components/MobileNavigation'
 
 export default function Header() {
 	return (
 		<header className="border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-			<div className="container mx-auto px-4 h-16 flex items-center justify-center">
-				<nav className="flex items-center w-full justify-between">
+			<div className="container mx-auto px-4 h-14 md:h-16 flex items-center justify-between md:justify-center">
+				<nav className="hidden md:flex items-center w-full justify-between">
 					<a href='/'>
 						<Image
 							src="https://res.cloudinary.com/dtzcyvuqi/image/upload/v1760590312/kioskopago_avnvtf.svg"
@@ -14,8 +15,9 @@ export default function Header() {
 							width={180}
 						/>
 					</a>
-					<HeaderMenu />
+					<DesktopNavigation />
 				</nav>
+				<MobileNavigation />
 			</div>
 		</header>
 	)
