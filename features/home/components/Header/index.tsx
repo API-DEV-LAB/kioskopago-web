52617f6aa495add3073699a5b346f236b7f72a2a
efrencodes <efrencodes@gmail.com>
2025-10-24 22:43:16 -0700

    feat: Update url of logo

--- features/home/components/Header/index.tsx
+++ features/home/components/Header/index.tsx
@@ -6,9 +6,9 @@ export default function Header() {
 		<header className="border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
 			<div className="container mx-auto px-4 h-16 flex items-center justify-center">
 				<nav className="flex items-center w-full justify-between">
-					<a href='/'>
+					<a href="/">
 						<Image
-							src="https://res.cloudinary.com/dtzcyvuqi/image/upload/v1760590312/kioskopago_avnvtf.svg"
+							src="https://res.cloudinary.com/dtzcyvuqi/image/upload/v1761367317/logo-kioskopago.svg"
 							alt="Logo Kioskopago"
 							height={60}
 							width={180}
