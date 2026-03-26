fb8db7c95b4204f9d2a28fcb432c8f22b56d9982
efrencodes <efrencodes@gmail.com>
2026-03-25 21:53:09 -0700

    chore: Remove basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  basePath: '/kiosko',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
+	output: 'standalone',
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	images: {
+		unoptimized: true,
+	},
 }
 
 export default nextConfig
